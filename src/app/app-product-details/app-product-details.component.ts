a683a30483ecfd0a60431faeda95cc3958f68703
chloehd <chloehipeau@gmail.com>
2020-02-17 14:44:50 +0100

    1 file modified, 2 added

---
+++ src/app/app-product-details/app-product-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-product-details',
+  templateUrl: './app-product-details.component.html',
+  styleUrls: ['./app-product-details.component.css']
+})
+export class AppProductDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
